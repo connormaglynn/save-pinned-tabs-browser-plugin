d06ad4d1ae9c8fea3040ebf6d67846e84bd512f3
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-04-19 21:35:51 +0100

    ✨ Add logic to store a group of pinned tabs

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("saveButton").addEventListener("click", async () => {
+    const groupName = document.getElementById("groupName").value
+
     const pinnedTabs = await chrome.tabs.query({ pinned: true })
-    const pinnedTabURLs = pinnedTabs.map((tab) => tab.url)
-    await chrome.storage.sync.set({ pinnedTabs: pinnedTabURLs })
-    console.log(`Pinned tabs saved successfully: [ ${Array.toString(pinnedTabURLs)} ]`)
+    const pinnedTabsUrls = pinnedTabs.map((tab) => tab.url)
+
+    var { groups } = await chrome.storage.sync.get(["groups"])
+    if (!groups) groups = []
+    groups.push({ name: groupName, pinnedTabsUrls: pinnedTabsUrls })
+
+    await chrome.storage.sync.set({ groups: groups })
+    console.log(`Pinned tabs saved successfully: [ ${Array.toString(groups)} ]`)
   })
 
   document.getElementById("displayPinnedTabsUrls").addEventListener("click", async () => {
-    const result = await chrome.storage.sync.get(["pinnedTabs"])
-    document.getElementById("pinnedTabsDisplay").textContent = JSON.stringify(result.pinnedTabs)
+    const result = await chrome.storage.sync.get(["groups"])
+    document.getElementById("pinnedTabsDisplay").textContent = JSON.stringify(result.groups)
   })
 
   document.getElementById("removePinnedTabsUrls").addEventListener("click", () => {
