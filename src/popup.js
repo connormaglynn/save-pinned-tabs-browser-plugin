5bf037098f66a86672ba2709af10a27d2b6b6d71
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-04-21 19:25:41 +0100

    ✨ Add search function

--- src/popup.js
+++ src/popup.js
@@ -31,7 +31,14 @@ document.addEventListener("DOMContentLoaded", async () => {
   })
 
   document.addEventListener("keypress", (event) => {
-    if (event.key === "Enter" && (event.target.classList.contains("group-item") || event.target.classList.contains("remove-button"))) event.target.click()
+    if (event.key === "Enter" && (event.target.classList.contains("group-item") || event.target.classList.contains("remove-button"))) {
+      event.target.click()
+    }
+
+    const searchBoxElement = document.getElementById("searchBox")
+    if (searchBoxElement !== document.activeElement) {
+      searchBoxElement.focus()
+    }
   })
 
   document.getElementById("saveButton").addEventListener("click", async () => {
@@ -47,6 +54,44 @@ document.addEventListener("DOMContentLoaded", async () => {
     await chrome.storage.sync.set({ groups: groups })
     console.log(`Pinned tabs saved successfully: [ ${Array.toString(groups)} ]`)
     await displayStoredGroups()
+  })
+
+  document.getElementById("searchBox").addEventListener("input", async () => {
+    const searchTerm = document.getElementById("searchBox").value
+
+    const groupsDisplay = document.getElementById("groupsDisplay")
+    while (groupsDisplay.firstChild) {
+      groupsDisplay.removeChild(groupsDisplay.lastChild)
+    }
+
+    if (!searchTerm) {
+      displayStoredGroups()
+      return
+    }
+
+    var { groups } = await chrome.storage.sync.get(["groups"])
+    if (!groups) groups = []
+
+    const filteredGroups = groups.filter((group) => group.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    filteredGroups.forEach((group, index) => {
+      const deleteElement = document.createElement("span")
+      deleteElement.dataset.groupId = group.id
+      deleteElement.classList.add("remove-button")
+      deleteElement.role = "button"
+      deleteElement.tabIndex = groups.length + index + 1
+      deleteElement.appendChild(document.createTextNode("Remove"))
+
+      const groupElement = document.createElement("div")
+      groupElement.dataset.id = group.id
+      groupElement.classList.add("group-item")
+      groupElement.role = "button"
+      groupElement.tabIndex = 0
+      groupElement.appendChild(document.createTextNode(group.name))
+
+      groupElement.appendChild(deleteElement)
+      groupsDisplay.appendChild(groupElement)
+    })
   })
 })
 
