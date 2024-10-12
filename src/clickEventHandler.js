0bc2a98896a32386440558a55df1f27f2af363e6
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-10-12 20:21:47 +0100

    ♻ Refactor `clickEventHandler` into Sperate File

---
+++ src/clickEventHandler.js
@@ -0,0 +1,65 @@
+class ClickEventHandler {
+  /** @param {groupRepository} groupRepository @param {GroupView} groupView @param {EditGroupView} editGroupView @param {object} browser @param {object} clickEvents **/
+  constructor(groupRepository, groupsView, editGroupView, browser, clickEvents) {
+    this.groupRepository = groupRepository
+    this.groupsView = groupsView
+    this.editGroupView = editGroupView
+    this.browser = browser
+    this.clickEvents = clickEvents
+  }
+  handle = async (event) => {
+    const target = event.target
+    const clickEvent = target.dataset.clickEvent
+
+    if (this.clickEvents.OPEN_GROUP_TABS_BY_GROUP_ID_ON_ELEMENT === clickEvent) {
+      const groupId = target.dataset.groupId
+      const oldPinnedTabs = await this.browser.tabs.query({ pinned: true, currentWindow: true })
+      const oldPinnedTabsIds = oldPinnedTabs.map((tab) => tab.id)
+
+      const newGroup = await this.groupRepository.findById(groupId)
+      const newPinnedTabsUrls = newGroup?.pinnedTabsUrls
+      newPinnedTabsUrls?.forEach(async (url) => {
+        await this.browser.tabs.create({
+          url: url,
+          pinned: true,
+        })
+      })
+
+      await this.browser.tabs.remove(oldPinnedTabsIds)
+    }
+
+    if (this.clickEvents.REMOVE_GROUP_BY_GROUP_ID_ON_ELEMENT === clickEvent) {
+      const groupIdToRemove = target.dataset.groupId
+      await this.groupRepository.removeById(groupIdToRemove)
+      await this.groupsView.refresh(await this.groupRepository.findAll())
+      this.editGroupView.close()
+    }
+
+    if (this.clickEvents.SAVE_GROUP_EDITS_BY_GROUP_ID_ON_ELEMENT === clickEvent) {
+      const groupIdToEdit = target.dataset.groupId
+      const edittedGroup = this.editGroupView.getValues()
+
+      await this.groupRepository.update(new GroupEntity(groupIdToEdit, edittedGroup.name, edittedGroup.pinnedTabsUrls))
+      await this.groupsView.refresh(await this.groupRepository.findAll())
+      this.editGroupView.close()
+    }
+
+    if (this.clickEvents.CREATE_NEW_GROUP_FROM_CURRENT_PINNED_TABS === clickEvent) {
+      const groupName = document.getElementById("groupName").value
+      const pinnedTabs = await this.browser.tabs.query({ pinned: true, currentWindow: true })
+      const pinnedTabsUrls = pinnedTabs.map((tab) => tab.url)
+
+      await this.groupRepository.add(new GroupModel(groupName, pinnedTabsUrls))
+      await this.groupsView.refresh(await this.groupRepository.findAll())
+    }
+
+    if (this.clickEvents.OPEN_EDIT_VIEW_BY_GROUP_ID_ON_ELEMENT === clickEvent) {
+      const group = await this.groupRepository.findById(target.dataset.groupId)
+      this.editGroupView.open(group)
+    }
+
+    if (this.clickEvents.CLOSE_EDIT_VIEW === clickEvent) {
+      this.editGroupView.close()
+    }
+  }
+}
--- src/popup.js
+++ src/popup.js
@@ -1,76 +1,25 @@
-const clickEvents = {
-  OPEN_GROUP_TABS_BY_GROUP_ID_ON_ELEMENT: "OPEN_GROUP_TABS_BY_GROUP_ID_ON_ELEMENT",
-  REMOVE_GROUP_BY_GROUP_ID_ON_ELEMENT: "REMOVE_GROUP_BY_GROUP_ID_ON_ELEMENT",
-  SAVE_GROUP_EDITS_BY_GROUP_ID_ON_ELEMENT: "SAVE_GROUP_EDITS_BY_GROUP_ID_ON_ELEMENT",
-  OPEN_EDIT_VIEW_BY_GROUP_ID_ON_ELEMENT: "OPEN_EDIT_VIEW_BY_GROUP_ID_ON_ELEMENT",
-  CLOSE_EDIT_VIEW: "CLOSE_EDIT_VIEW",
-  CREATE_NEW_GROUP_FROM_CURRENT_PINNED_TABS: "CREATE_NEW_GROUP_FROM_CURRENT_PINNED_TABS",
-}
 document.addEventListener("DOMContentLoaded", async () => {
+  const clickEvents = {
+    OPEN_GROUP_TABS_BY_GROUP_ID_ON_ELEMENT: "OPEN_GROUP_TABS_BY_GROUP_ID_ON_ELEMENT",
+    REMOVE_GROUP_BY_GROUP_ID_ON_ELEMENT: "REMOVE_GROUP_BY_GROUP_ID_ON_ELEMENT",
+    SAVE_GROUP_EDITS_BY_GROUP_ID_ON_ELEMENT: "SAVE_GROUP_EDITS_BY_GROUP_ID_ON_ELEMENT",
+    OPEN_EDIT_VIEW_BY_GROUP_ID_ON_ELEMENT: "OPEN_EDIT_VIEW_BY_GROUP_ID_ON_ELEMENT",
+    CLOSE_EDIT_VIEW: "CLOSE_EDIT_VIEW",
+    CREATE_NEW_GROUP_FROM_CURRENT_PINNED_TABS: "CREATE_NEW_GROUP_FROM_CURRENT_PINNED_TABS",
+  }
+
   const browser = chrome
   const groupRepository = new GroupRepository(browser)
   const groupsView = new GroupView(groupRepository, clickEvents)
   const editGroupView = new EditGroupView()
+  const clickEventHandler = new ClickEventHandler(groupRepository, groupsView, editGroupView, browser, clickEvents)
 
   await groupRepository.removeUnlinkedGroups()
   await groupsView.open(await groupRepository.findAll())
 
   document.addEventListener("click", async (event) => {
-    const target = event.target
-    const clickEvent = target.dataset.clickEvent
-
-    console.info(`🖱️ ClickEvent Triggered | clickEvent on Target [ ${clickEvent} ]`)
-
-    if (clickEvents.OPEN_GROUP_TABS_BY_GROUP_ID_ON_ELEMENT === clickEvent) {
-      const groupId = target.dataset.groupId
-      const oldPinnedTabs = await browser.tabs.query({ pinned: true, currentWindow: true })
-      const oldPinnedTabsIds = oldPinnedTabs.map((tab) => tab.id)
-
-      const newGroup = await groupRepository.findById(groupId)
-      const newPinnedTabsUrls = newGroup?.pinnedTabsUrls
-      newPinnedTabsUrls?.forEach(async (url) => {
-        await browser.tabs.create({
-          url: url,
-          pinned: true,
-        })
-      })
-
-      await browser.tabs.remove(oldPinnedTabsIds)
-    }
-
-    if (clickEvents.REMOVE_GROUP_BY_GROUP_ID_ON_ELEMENT === clickEvent) {
-      const groupIdToRemove = target.dataset.groupId
-      await groupRepository.removeById(groupIdToRemove)
-      await groupsView.refresh(await groupRepository.findAll())
-      editGroupView.close()
-    }
-
-    if (clickEvents.SAVE_GROUP_EDITS_BY_GROUP_ID_ON_ELEMENT === clickEvent) {
-      const groupIdToEdit = target.dataset.groupId
-      const edittedGroup = editGroupView.getValues()
-
-      await groupRepository.update(new GroupEntity(groupIdToEdit, edittedGroup.name, edittedGroup.pinnedTabsUrls))
-      await groupsView.refresh(await groupRepository.findAll())
-      editGroupView.close()
-    }
-
-    if (clickEvents.CREATE_NEW_GROUP_FROM_CURRENT_PINNED_TABS === clickEvent) {
-      const groupName = document.getElementById("groupName").value
-      const pinnedTabs = await browser.tabs.query({ pinned: true, currentWindow: true })
-      const pinnedTabsUrls = pinnedTabs.map((tab) => tab.url)
-
-      await groupRepository.add(new GroupModel(groupName, pinnedTabsUrls))
-      await groupsView.refresh(await groupRepository.findAll())
-    }
-
-    if (clickEvents.OPEN_EDIT_VIEW_BY_GROUP_ID_ON_ELEMENT === clickEvent) {
-      const group = await groupRepository.findById(target.dataset.groupId)
-      editGroupView.open(group)
-    }
-
-    if (clickEvents.CLOSE_EDIT_VIEW === clickEvent) {
-      editGroupView.close()
-    }
+    console.info(`🖱️ ClickEvent Triggered | clickEvent on Target [ ${event.target.dataset.clickEvent} ]`)
+    clickEventHandler.handle(event)
   })
 
   document.addEventListener("keypress", (event) => {
