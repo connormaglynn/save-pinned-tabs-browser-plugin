07314b67a1790b6a7af6bcc1d51c895d82067638
connormaglynn <connor.glynn@digital.justice.gov.uk>
2024-10-18 21:00:23 +0100

    ✨ Add Drag and Drop Re-Ordering in `EditGroupView`

--- src/views/editGroupView.js
+++ src/views/editGroupView.js
@@ -2,7 +2,9 @@ import { GroupEntity } from "../repositories/groupRepository.js"
 import { PreferencesModel } from "../repositories/preferencesRepository.js"
 
 export class EditGroupView {
-  contructor() { }
+  constructor() {
+    this.editGroupUrlsListView = new EditGroupUrlsListView()
+  }
 
   /** @param {GroupEntity>} group @param {PreferencesModel} preferences  */
   open(group, preferences) {
@@ -24,17 +26,7 @@ export class EditGroupView {
     const editGroupNameElement = document.getElementById("edit-group-name")
     editGroupNameElement.value = group?.name
 
-    const editGroupsUrlsList = document.getElementById("editGroupsUrlsList")
-    while (editGroupsUrlsList.firstChild) {
-      editGroupsUrlsList.removeChild(editGroupsUrlsList.lastChild)
-    }
-    group?.pinnedTabsUrls?.forEach((url) => {
-      const urlElement = document.createElement("input")
-      urlElement.classList.add("edit-url")
-      urlElement.value = url
-
-      editGroupsUrlsList.appendChild(urlElement)
-    })
+    this.editGroupUrlsListView.replace(group.pinnedTabsUrls)
   }
 
   close() {
@@ -57,5 +49,75 @@ export class EditGroupView {
       group: new GroupEntity(groupId, newName, newPinnedTabsUrls),
       isLoadOnStartup: isLoadOnStartup,
     }
+  }
+}
+
+export class EditGroupUrlsListView {
+  constructor() {
+    this.id = "editGroupsUrlsList"
+  }
+
+  /** @param {Array<string>} urls  */
+  replace(urls) {
+    this.close()
+    this.open(urls)
+  }
+
+  /** @param {Array<string>} urls  */
+  open(urls) {
+    const editGroupsUrlsList = document.getElementById(this.id)
+    urls.forEach((url, index) => {
+      this.appendUrlInputTextBox(editGroupsUrlsList, url, index)
+    })
+  }
+
+  close() {
+    const editGroupsUrlsList = document.getElementById(this.id)
+    while (editGroupsUrlsList.firstChild) {
+      editGroupsUrlsList.removeChild(editGroupsUrlsList.lastChild)
+    }
+  }
+
+  /** @param {HTMLElement} editGroupsUrlsListElement @param {string} url  @param {number} index **/
+  appendUrlInputTextBox(editGroupsUrlsListElement, url, index) {
+    const urlElement = document.createElement("input")
+    urlElement.classList.add("edit-url")
+    urlElement.value = url
+    urlElement.dataset.index = index
+    urlElement.draggable = true
+    urlElement.addEventListener("dragstart", this.dragStartHandler)
+    urlElement.ondrop = this.dropHandler
+    urlElement.ondragover = this.dragoverHandler
+
+    editGroupsUrlsListElement.appendChild(urlElement)
+  }
+
+  /** @returns {Array<string>} **/
+  getValues() {
+    const newPinnedTabsUrls = []
+    document.getElementById(this.id).childNodes.forEach((child) => newPinnedTabsUrls.push(child.value))
+    return newPinnedTabsUrls
+  }
+
+  dragStartHandler = (event) => {
+    console.debug(`🐛 dragStartHandler() - index ${event.target.dataset.index}`)
+    event.dataTransfer.setData('text', event.target.dataset.index);
+    event.dataTransfer.dropEffect = "move"
+  }
+
+  dragoverHandler = (event) => {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = "move"
+  }
+
+  dropHandler = async (event) => {
+    event.preventDefault()
+    const movingItemIndex = event.dataTransfer.getData("text")
+    const targetItemIndex = event.target.dataset.index
+    const urls = this.getValues()
+    const movingItemUrl = urls[movingItemIndex]
+    const filteredUrls = urls.filter((_, index) => Number(movingItemIndex) !== index)
+    const newUrlOrder = filteredUrls.toSpliced(targetItemIndex, 0, movingItemUrl)
+    this.replace(newUrlOrder)
   }
 }
